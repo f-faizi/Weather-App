2e12deab48d1df5ff49bf7ae574dd6b0ca220a2c
obaidahmadzai <obaidullahkhan206@gmail.com>
2023-03-01 18:36:12 +0430

    fix: :bug: conflict between the formatCurrentWeather function and the variable formatCurrentWeather
    The error is caused by a naming conflict between the formatCurrentWeather function and the variable formatCurrentWeather that's declared inside the getFormatteredWeatherData function.  In the getFormatteredWeatherData function, you're using formatCurrentWeather as a variable to store the result of the getWeatherData function after formatting the weather data using the formatCurrentWeather function. However, since you've already declared a function with the same name at the top of the file, the JavaScript interpreter is unable to distinguish between the two and throws an error.  To fix this issue, you should rename the variable formatCurrentWeather to something else, like formattedWeatherData

--- src/Services/weatherServices.js
+++ src/Services/weatherServices.js
@@ -1,34 +1,53 @@
 const API_KEY = "40325383408f891c2a7adcf5db90a594";
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q=Kabul&appid=40325383408f891c2a7adcf5db90a594#"
+const BASE_URL =
+  "https://api.openweathermap.org/data/2.5/weather?q=Kabul&appid=40325383408f891c2a7adcf5db90a594#";
 
 const getWeatherData = (infoType, searchParams) => {
-    const url = new URL(BASE_URL + "/" + infoType);
-    url.search = new URLSearchParams({...searchParams, appid:API_KEY})
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
 
-    return fetch(url).then((res) => res.json());
+  return fetch(url).then((res) => res.json());
 };
+
 const formatCurrentWeather = (data) => {
-    const {
-        coord: {lat, lon},
-        main: {temp, feels_like, temp_min, temp_max, humidity},
-        name,
-        dt,
-        sys: {country, sunrise, sunset},
-        weather,
-        wind: {speed}
-    } = data
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
 
-    const {main: details, icon} = weather[0]
+  const { main: details, icon } = weather[0];
 
-    return {lat, lon, temp, feels_like, temp_max,temp_min, humidity, name, dt, country, sunrise, sunset, details, icon, speed}
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_max,
+    temp_min,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
 
-}
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedWeatherData = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
 
-const getFormatteredWeatherData = async (searchParams) => {
-    const formatCurrentWeather = await getWeatherData('weather', searchParams).then(formatCurrentWeather);
+  return formattedWeatherData;
+};
 
-    return formatCurrentWeather
-}
-
-
-export default getFormatteredWeatherData
+export default getFormattedWeatherData;
